ad044443e647a45b82e6c855fc133ebb54889ada
rad304 <y.radouani929@gmail.com>
2020-03-18 20:22:10 +0100

    Main features

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <b-container class="mt-5">
+      <b-form inline>
+        <label class="mr-sm-2" for="inline-form-custom-select-pref">Country</label>
+        <b-form-select
+          id="inline-form-custom-select-pref"
+          class="mb-2 mr-sm-2 mb-sm-0"
+          :options="options"
+          v-model="selected"
+        ></b-form-select>
+        <b-button variant="primary" @click="getStats($event)">
+          Get Stats
+        </b-button>
+      </b-form>
+      <b-row class="mt-5">
+        <b-col md="4" class="mt-2">
+          <b-card
+          >
+            <b-card-body>
+              <b-card-title>😔<br>Confirmed:</b-card-title>
+              <b-card-sub-title class="mb-2">{{ confirmed }}</b-card-sub-title>
+            </b-card-body>
+          </b-card>  
+        </b-col>
+        <b-col md="4" class="mt-2">
+          <b-card
+          >
+            <b-card-body>
+              <b-card-title>😢<br>Deaths:</b-card-title>
+              <b-card-sub-title class="mb-2">{{ deaths }}</b-card-sub-title>
+            </b-card-body>
+          </b-card>  
+        </b-col>
+        <b-col md="4" class="mt-2 mb-2">
+          <b-card
+          >
+            <b-card-body>
+              <b-card-title>😷<br>Recovered:</b-card-title>
+              <b-card-sub-title class="mb-2">{{ recovered }}</b-card-sub-title>
+            </b-card-body>
+          </b-card>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeComponent',
+  props: ['msg'],
+  data() {
+    return {
+      options: [],
+      data: null,
+      selected: null,
+      confirmed: 0,
+      deaths: 0,
+      recovered: 0,
+    }
+  },
+  methods: {
+    sortArray(){
+        return this.options.sort((a, b) => a > b );
+    },
+    getStats(){
+        var countryStats = this.data[this.selected];
+        this.confirmed =countryStats[countryStats.length-1].confirmed;
+        this.deaths =countryStats[countryStats.length-1].deaths;
+        this.recovered =countryStats[countryStats.length-1].recovered;
+    }
+  },
+  mounted () {
+    this.$http
+      .get('https://pomber.github.io/covid19/timeseries.json')
+      .then(response => {
+        this.data = response.data
+        Object.keys(response.data).forEach((key)=>{
+          this.options.push(key);
+        })
+      })
+      .catch(error => {
+        console.log(error)
+      })
+      .finally(()=>{
+        this.sortArray()
+      });
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
